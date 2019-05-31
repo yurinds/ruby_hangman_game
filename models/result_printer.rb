57f480561702b402abcdc8a3f847a09635a420f3
Dmitrii Iurin <dimon07710@yandex.ru>
2019-06-01 01:18:56 +0300

    added print hangman picture, if letter is wrong

--- models/result_printer.rb
+++ models/result_printer.rb
@@ -17,7 +17,7 @@ class ResultPrinter
     cls
 
     puts "Слово: #{get_letters_for_print(game.good_letters, game.letters)}"
-
+    puts print_hangman(game.errors)
     puts "Ошибки (#{game.errors}): #{game.bad_letters.join(', ')}"
 
     if game.status == -1
@@ -33,5 +33,124 @@ class ResultPrinter
   # очищаем экран консоли
   def cls
     system('cls') || system('clear')
+  end
+
+  def print_hangman(errors)
+    case errors
+    when 0
+      puts "
+            _______
+            |/
+            |
+            |
+            |
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+    when 1
+      puts "
+          _______
+          |/
+          |     ( )
+          |
+          |
+          |
+          |
+          |
+          |
+        __|________
+        |         |
+        "
+    when 2
+      puts "
+          _______
+          |/
+          |     ( )
+          |      |
+          |
+          |
+          |
+          |
+          |
+        __|________
+        |         |
+        "
+    when 3
+      puts "
+          _______
+          |/
+          |     ( )
+          |      |_
+          |        \\
+          |
+          |
+          |
+          |
+        __|________
+        |         |
+        "
+    when 4
+      puts "
+          _______
+          |/
+          |     ( )
+          |     _|_
+          |    /   \\
+          |
+          |
+          |
+          |
+        __|________
+        |         |
+        "
+    when 5
+      puts "
+          _______
+          |/
+          |     ( )
+          |     _|_
+          |    / | \\
+          |      |
+          |
+          |
+          |
+        __|________
+        |         |
+        "
+
+    when 6
+      puts "
+          _______
+          |/
+          |     ( )
+          |     _|_
+          |    / | \\
+          |      |
+          |     / \\
+          |    /   \\
+          |
+        __|________
+        |         |
+        "
+    when 7
+      puts "
+          _______
+          |/     |
+          |     (_)
+          |     _|_
+          |    / | \\
+          |      |
+          |     / \\
+          |    /   \\
+          |
+        __|________
+        |         |
+        "
+
     end
+  end
 end
