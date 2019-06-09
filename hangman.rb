c4d64e49d096b94a396bc882396db0b10bb2fcae
Dmitrii Iurin <dimon07710@yandex.ru>
2019-06-09 18:16:40 +0300

    add show random words for game from words.txt

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,20 @@
-require './models/game.rb'
-require './models/result_printer.rb'
+require_relative './models/game.rb'
+require_relative './models/result_printer.rb'
 
-word = ARGV[0]
+current_path = File.dirname(__FILE__)
 
-if [nil, ''].include?(word)
-  puts 'Программа принудительно закрыта. Введите слово в качестве параметра при запуске программы!'
+file_path = current_path + '/data/words.txt'
+
+if File.exist?(file_path)
+  new_file = File.new(file_path, 'r:UTF-8')
+  lines = new_file.readlines
+  abort 'В текстовом файле (./data/words.txt) отсутствуют слова для игры!' if lines.empty?
+else
+  puts 'Программа принудительно закрыта. В пути ./data/words.txt отсутствует файл'
   exit
 end
+
+word = lines.sample.strip
 
 result_printer = ResultPrinter.new
 game           = Game.new(word)
