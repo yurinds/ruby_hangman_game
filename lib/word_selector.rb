f25452c608081ce03b095992e1809845df71bbc6
Dmitrii Iurin <dimon07710@yandex.ru>
2019-07-13 14:19:36 +0300

    WordSelector refactoring

--- lib/word_selector.rb
+++ lib/word_selector.rb
@@ -6,23 +6,18 @@ class WordSelector
   end
 
   def self.from_file(current_path)
-
     file_path = current_path + '/data/words.txt'
 
     begin
-      new_file = File.new(file_path, 'r:UTF-8')
+      lines = File.readlines(file_path, chomp: true)
     rescue SystemCallError => exception
-      puts 'Не удалось прочитать файл ' + exception.message
       return nil
     end
 
-    lines = new_file.readlines
-    new_file.close
-
-    word = if !lines.empty?
-             lines.sample.strip
-           else
+    word = if lines.empty?
              ''
+           else
+             lines.sample.strip
            end
 
     new(word)
